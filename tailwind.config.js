6adb0b947d88b45b2936050503d3cc5c93486d40
amitkaill <amitkaill1989@gmail.com>
2024-02-28 23:23:14 -0400

    por hoy

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    colors:{
+      'White': "hsl(0, 0%, 100%)",
+      'Grey': "hsl(0, 0%, 50%)",
+      'Black': "hsl(0, 0%, 7%)",
+      'Yellow': 'hsl(47, 88%, 63%)',
+    },
+    extend: {
+      boxShadow: {
+        'am1': '4px 3px 3px 4px rgba(0, 0, 0, 0.3)',
+      }
+    },
   },
   plugins: [],
 }
